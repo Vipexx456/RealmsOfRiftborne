c6d09890e63d2a07bec0a1010f94e6894acc3366
Mysteray01 <rayartmasbate@gmail.com>
2025-09-27 01:26:23 +0800

    Added Characters with stats

---
+++ Character.java
@@ -0,0 +1,13 @@
+public class Character {
+    private int hp;
+    private int attack;
+    private int mana;
+    private int defense;
+    private int speed;
+    private int level;
+    private int experience;
+    private String name;
+    private String charClass;
+    private String weapon;
+
+}
---
+++ Gunner.java
@@ -0,0 +1,26 @@
+public class Gunner {
+    private int hp;
+    private int attack;
+    private int mana;
+    private int defense;
+    private int speed;
+    private int level;
+    private int experience;
+    private String name;
+    private String charClass;
+    private String weapon;
+
+    public Gunner() {
+        this.hp = 4000;
+        this.attack = 450;
+        this.mana = 600;
+        this.defense = 125;
+        this.speed = 100;
+        this.level = 1;
+        this.experience = 0;
+        this.name = "Selene Arclight";
+        this.charClass = "Gunner";
+        this.weapon = "Rusty Dual Pistol";
+    }
+    
+}
